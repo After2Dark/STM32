e910a1e186533e642ec94dbaf7a8243cb96b95a6
BAM-BCEM-KAPYT <andrew.kovalenko.2001@mail.ru>
2020-01-12 14:56:18 +0300

    Revert "Revert "only display_value""
    This reverts commit 1c4ffd73f5b4a8fe84cc9cea3bf38ad2471c0799.

---
+++ 7th-segment_ind/Debug/Src/indicator.d
@@ -0,0 +1,20 @@
+Src/indicator.o: ../Src/indicator.c ../Inc/stm32f767xx.h \
+ F:/Cube/7th-segment_ind/Inc/CMSIS/core_cm7.h \
+ F:/Cube/7th-segment_ind/Inc/CMSIS/cmsis_version.h \
+ F:/Cube/7th-segment_ind/Inc/CMSIS/cmsis_compiler.h \
+ F:/Cube/7th-segment_ind/Inc/CMSIS/cmsis_gcc.h \
+ F:/Cube/7th-segment_ind/Inc/CMSIS/mpu_armv7.h ../Inc/system_stm32f7xx.h
+
+../Inc/stm32f767xx.h:
+
+F:/Cube/7th-segment_ind/Inc/CMSIS/core_cm7.h:
+
+F:/Cube/7th-segment_ind/Inc/CMSIS/cmsis_version.h:
+
+F:/Cube/7th-segment_ind/Inc/CMSIS/cmsis_compiler.h:
+
+F:/Cube/7th-segment_ind/Inc/CMSIS/cmsis_gcc.h:
+
+F:/Cube/7th-segment_ind/Inc/CMSIS/mpu_armv7.h:
+
+../Inc/system_stm32f7xx.h:
---
+++ 7th-segment_ind/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c ../Src/indicator.h
+
+../Src/indicator.h:
